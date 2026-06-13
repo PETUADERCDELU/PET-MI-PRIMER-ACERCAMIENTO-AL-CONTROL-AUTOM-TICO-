6688bdfdf23e7297d70e5e6f862219acb81937a4
FDavidGallo <134020304+FDavidGallo@users.noreply.github.com>
2026-06-13 16:39:56 -0300

    0.0.0.6 BUG RELE SIMPLE
    La logica del rele de la versión simplificada estaba mal planteada, el rele de la versión simple es de activación por bajo

--- PID DIGITAL/Versión simple/Seccion Micro/Version_simplificada_copy_20260528133420/Version_simplificada_copy_20260528133420.ino
+++ PID DIGITAL/Versión simple/Seccion Micro/Version_simplificada_copy_20260528133420/Version_simplificada_copy_20260528133420.ino
@@ -7,7 +7,7 @@
 // ==================== PARÁMETROS Y VARIABLES GLOBALES ====================
 float setpoint = 30.0;
 float KP = 2.0;
-float KI = 0.1;
+float KI = 0.0;
 float KD = 1.0;
 double TS = 1.0;          // Tiempo de muestreo por defecto en segundos
 double HISTERESIS = 2.0;  // Ancho de banda para el control On/Off
@@ -29,6 +29,7 @@ void enviarTelemetria();
 // ==================== SETUP ====================
 void setup() {
     Serial.begin(9600);
+  //  Serial.println("Inicio");
     delay(200);
     SensorDht11.inicializar();
     // Apagar el actuador al inicio de forma segura (0% de potencia)
@@ -42,6 +43,7 @@ void setup() {
 // ==================== LOOP PRINCIPAL ====================
 void loop() {
     // 1. Gestión de comandos por puerto serie
+ //   temperaturaActual = SensorDht11.leerTemperatura();
     if (Serial.available()) {
         String cmd = Serial.readStringUntil('\n');
         cmd.trim();
@@ -54,7 +56,7 @@ void loop() {
     unsigned long intervaloMs = (unsigned long)(TS * 1000.0);
     if (millis() - tiempoUltimoControl >= intervaloMs) {
         tiempoUltimoControl = millis(); // Actualizar el marcador de tiempo
-
+        
         // Leer el sensor de temperatura
         float lectura = SensorDht11.leerTemperatura();
         // Validar que la lectura sea correcta antes de aplicar el lazo de control
@@ -64,17 +66,17 @@ void loop() {
             double accionControl = sistemaControl.CalcularError(setpoint, temperaturaActual);
             // Mapear y acotar la salida al rango que acepta el Actuador [1, 100]
             porcentajeActuador = (int)accionControl;
-            if (porcentajeActuador < 1)   porcentajeActuador = 1;
+            if (porcentajeActuador < 1)   porcentajeActuador = 0;
             if (porcentajeActuador > 100) porcentajeActuador = 100;
             // Aplicar el control al Relé
             ReleSsr.Aplicar(porcentajeActuador);
             // Enviar los datos del sistema
             enviarTelemetria();
         } else {
-            delay(2);//Serial.println("[ERROR] No se pudo calcular el control debido a una falla en el sensor.");
+            delay(200);//Serial.println("[ERROR] No se pudo calcular el control debido a una falla en el sensor.");
         }
     }
-    
+       enviarTelemetria();
     delay(10); // Pequeña pausa para estabilidad del microcontrolador
 }
 
--- PID DIGITAL/Versión simple/Seccion Micro/Version_simplificada_copy_20260528133420/rele.cpp
+++ PID DIGITAL/Versión simple/Seccion Micro/Version_simplificada_copy_20260528133420/rele.cpp
@@ -7,18 +7,20 @@ Rele::Rele(int pin, int BaseDeTiempo, String Tipo)
   pinMode(_pin, OUTPUT);
   digitalWrite(_pin, LOW);   // Inicialmente apagado
 }
-
+ // CHE ESTO LO CORREGÍ, EL RELE ES DE ACTIVACION POR BAJO
+ // NO TOCAR!!!
 // Método Aplicar
 void Rele::Aplicar(int porcentaje) {
   // Validar porcentaje (1 a 100)
-  if (porcentaje < 1) porcentaje = 1;
+  if (porcentaje < 1) porcentaje = 0;
   if (porcentaje > 100) porcentaje = 100;
 
-  int baseTiempo = getBaseDeTiempo();  // en milisegundos
-  unsigned long tiempoOn = (unsigned long)porcentaje * baseTiempo / 100;
-  unsigned long tiempoOff = baseTiempo - tiempoOn;
+  int baseTiempo = 800;  // en milisegundos
+  unsigned long tiempoOff = (unsigned long)porcentaje * baseTiempo / 100;
+  unsigned long tiempoOn = baseTiempo - tiempoOff;
 
   // Lógica de encendido/apagado dentro de la base de tiempo
+ 
   digitalWrite(_pin, HIGH);   // Enciende el relé
   delay(tiempoOn);            // Permanece encendido
   digitalWrite(_pin, LOW);    // Apaga el relé
